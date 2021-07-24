2d20d4a413ddab16f966ecadbc0a1e6e17b6e6d7
Ryan Hartlage <ryanplusplus@gmail.com>
2021-07-23 21:22:47 -0400

    Revert heartbeat to work on pico

--- src/heartbeat.c
+++ src/heartbeat.c
@@ -6,31 +6,18 @@
 #include "heartbeat.h"
 #include "pico/stdlib.h"
 
-enum {
-  red_pin = 18,
-  green_pin = 19,
-  blue_pin = 20,
-};
-
 static void blink(tiny_timer_group_t* group, void* context)
 {
   (void)group;
   (void)context;
   static bool state;
-  gpio_put(red_pin, state = !state);
+  gpio_put(PICO_DEFAULT_LED_PIN, state = !state);
 }
 
 void heartbeat_init(tiny_timer_group_t* timer_group)
 {
-  gpio_init(red_pin);
-  gpio_init(green_pin);
-  gpio_init(blue_pin);
-  gpio_set_dir(red_pin, GPIO_OUT);
-  gpio_set_dir(green_pin, GPIO_OUT);
-  gpio_set_dir(blue_pin, GPIO_OUT);
-  gpio_put(red_pin, 1);
-  gpio_put(green_pin, 1);
-  gpio_put(blue_pin, 1);
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
   static tiny_timer_t heartbeat_timer;
   tiny_timer_start_periodic(timer_group, &heartbeat_timer, 500, blink, NULL);
