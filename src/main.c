6b3bc8f770fe822cb61d5497c083d6b92e875320
Ryan Hartlage <ryanplusplus@gmail.com>
2022-01-02 12:48:52 -0500

    Updated pico-tiny

--- src/main.c
+++ src/main.c
@@ -7,14 +7,14 @@
 #include <stdbool.h>
 #include "tiny_time_source.h"
 #include "tiny_timer.h"
-#include "heartbeat.h"
+#include "tiny_heartbeat.h"
 
 int main()
 {
   tiny_timer_group_t timer_group;
   tiny_timer_group_init(&timer_group, tiny_time_source_init());
 
-  heartbeat_init(&timer_group);
+  tiny_heartbeat_init(&timer_group);
 
   while(1) {
     tiny_timer_group_run(&timer_group);
